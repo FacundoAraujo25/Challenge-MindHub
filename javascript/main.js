af1e4e3dda6c9f5bd648775de86c10ea0381d5f6
FacundoAraujo25 <facundoaraujo96@gmail.com>
2022-04-15 11:39:17 -0300

    cambios en main js y html

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,35 @@
 Vue.createApp({
     data(){
         return{ 
+            productos_carrito:[],
+            productos:[]
 
         }
     },
     created(){
         fetch('https://apipetshop.herokuapp.com/api/articulos')
+        .then(response => response.json())
+            .then(data =>{
+                productos = data.response
+            })
     },
     methods:{
         modal_contacto(){
             let formulario = document.querySelector('#form')
             formulario.reset()
         }
+
     },
     computed:{
+
+        agregarProductos(){
+            for(i=0;i<2;i++){
+                if(!this.productos_carrito.includes(this.productos[i]))
+                {
+                    this.productos_carrito.push(this.productos[i])
+                }
+            }
+        }
 
     }
 }).mount('#app')
