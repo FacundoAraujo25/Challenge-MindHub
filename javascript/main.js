24838b524fa68f7333212484ccde5d52296db873
FacundoAraujo25 <facundoaraujo96@gmail.com>
2022-04-15 13:00:12 -0300

    cambios

--- javascript/main.js
+++ javascript/main.js
@@ -10,26 +10,33 @@ Vue.createApp({
         fetch('https://apipetshop.herokuapp.com/api/articulos')
         .then(response => response.json())
             .then(data =>{
-                productos = data.response
+                this.productos = data.response
+                
+                let productosEnStorage = JSON.parse(localStorage.getItem("carrito"))
+                if(productosEnStorage){
+                    this.productos_carrito = productosEnStorage
+                }
             })
+
+
     },
     methods:{
         modal_contacto(){
             let formulario = document.querySelector('#form')
             formulario.reset()
-        }
+        },
 
+        añadirAlCarrito(producto){
+            this.productos_carrito.push(producto)
+
+            localStorage.setItem("carrito",JSON.stringify(this.productos_carrito))
+        }
+        
+    
     },
     computed:{
+        
 
-        agregarProductos(){
-            for(i=0;i<2;i++){
-                if(!this.productos_carrito.includes(this.productos[i]))
-                {
-                    this.productos_carrito.push(this.productos[i])
-                }
-            }
-        }
 
     }
 }).mount('#app')
